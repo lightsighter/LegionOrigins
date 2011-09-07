303b56497c2613b760f7f7763e0508e60005367c
Sean Treichler <sjt@cs.stanford.edu>
2011-09-06 17:05:25 -0700

    initial (partial) version

---
+++ runtime/lowlevel.cc
@@ -0,0 +1,11 @@
+#include "lowlevel.h"
+
+// this is an implementation of the low level region runtime on top of GASnet+pthreads+CUDA
+
+namespace RegionRuntime {
+  namespace LowLevel {
+
+
+  }; // namespace LowLevel
+}; // namespace RegionRuntime
+
---
+++ runtime/lowlevel.h
@@ -0,0 +1,137 @@
+#ifndef RUNTIME_LOWLEVEL_H
+#define RUNTIME_LOWLEVEL_H
+
+#include <string>
+
+namespace RegionRuntime {
+  namespace LowLevel {
+
+    class Event {
+    public:
+      Event(const Event& copy_from);
+      Event(const Event& event1, const Event& event2);
+
+      void wait(void);
+
+    protected:
+      Event(unsigned _event_id);
+      
+      void trigger(void);
+
+    protected:
+      unsigned event_id;
+    };
+
+    class Lock {
+    public:
+      Lock(const Lock& copy_from);
+
+      Event lock(unsigned mode = 0, bool exclusive = true);
+      void unlock(void);
+
+    protected:
+      unsigned lock_id;
+    };
+
+    class Memory;
+
+    template <class T>
+    class RegionAllocator;
+
+    template <class T>
+    class RegionInstance;
+
+    template <class T>
+    struct ptr_t { unsigned value; };
+
+    template <class T>
+    class RegionMetaData {
+    public:
+      RegionMetaData(const std::string& _name, size_t _num_elements, Memory *_master_location);
+      ~RegionMetaData(void);
+
+      RegionAllocator<T> *create_region_allocator(Memory *location);
+      RegionInstance<T> *create_region_instance(Memory *location);
+    };
+
+    // untyped version of allocator that has all the smarts
+    class RegionAllocatorBase {
+    public:
+      virtual unsigned alloc(void) = 0;
+      virtual void free(unsigned ptr) = 0;
+    };
+
+    template <class T>
+    class RegionAllocator {
+    protected:
+      RegionAllocator(const std::string& _name, RegionMetaData<T> *_metadata, RegionAllocatorBase *_base);
+      virtual ~RegionAllocator(void);
+
+    public:
+      ptr_t<T> alloc(void) { return ptr_t<T>(base->alloc()); }
+      void free(ptr_t<T> ptr) { base->free(ptr.value); }
+
+    protected:
+      std::string name;
+      RegionMetaData<T> *metadata;
+      RegionAllocatorBase *base;
+    };
+
+    class RegionInstanceBase {
+    };
+
+    template <class T>
+    class RegionInstance {
+    protected:
+      RegionInstance(const std::string& _name, RegionMetaData<T> *_metadata, RegionInstanceBase *_base);
+      virtual ~RegionInstance(void);
+
+    public:
+      T read(ptr_t<T> ptr) { T temp; base->read(ptr.value, (unsigned char *)&temp); return temp; }
+      virtual void write(ptr_t<T> ptr, T newval) = 0;
+
+      typedef T (*reduction_operator)(T origval, T newval);
+      virtual void reduce(ptr_t<T> ptr, reduction_operator op, T newval) = 0;
+
+      virtual Event copy_to(RegionInstance<T> *dest, Event wait_on) = 0;
+    };
+
+    class Processor {
+    protected:
+      Processor(const std::string& _name);
+      virtual ~Processor(void);
+
+    public:
+      template <class T>
+      Event spawn(funcptr task, T args, Event wait_on)
+    };
+
+    class Memory {
+    protected:
+      Memory(const std::string& _name, size_t _size);
+      virtual ~Memory(void);
+
+    protected:
+      template <class T> friend class RegionMetadata<T>;
+
+      virtual RegionAllocatorBase *create_region_allocator_base(void /*FIX*/) = 0;
+
+      virtual RegionInstanceBase *create_region_instance_base(void /*FIX*/) = 0;
+    };
+
+    class Machine {
+    protected:
+      Machine(void);
+      ~Machine(void);
+
+    public:
+      static Machine *get_machine(void);
+
+      const std::set<Memory *>& all_memories(void);
+      const std::set<Processor *>& all_processors(void);
+    };
+
+  }; // namespace LowLevel
+}; // namespace RegionRuntime
+
+#endif
