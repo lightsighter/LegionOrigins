19ea62bb183bf3d143dbdb49b713b135325ae9fc
Sean Treichler <sjt@cs.stanford.edu>
2011-10-29 23:52:22 -0700

    changes

--- runtime/highlevel.h
+++ runtime/highlevel.h
@@ -86,6 +86,12 @@ namespace RegionRuntime {
 
     struct RegionRequirement {
     public:
+      RegionRequirement(void) {}
+      RegionRequirement(LogicalHandle _handle, AccessMode _mode,
+			AllocateMode _alloc, CoherenceProperty _prop,
+			LogicalHandle _parent)
+        : handle(_handle), mode(_mode), alloc(_alloc), 
+	  prop(_prop), parent(_parent) {}
       LogicalHandle handle;
       AccessMode mode;
       AllocateMode alloc;
@@ -1018,7 +1024,7 @@ namespace RegionRuntime {
 #endif
           continue;
         }
-        region = (LogicalHandle)LowLevel::RegionMetaData<T>::create_region(*mem_it,num_elmts);	
+        region = (LogicalHandle)LowLevel::RegionMetaData<T>::create_region(/*SJT*mem_it,*/num_elmts);	
         if (region.exists())
         {
           found = true;
