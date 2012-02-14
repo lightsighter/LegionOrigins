77a47d4b7ef4050c93231dc0dc45d913fbf75286
Mike Bauer <mebauer@cs.stanford.edu>
2012-02-13 18:12:46 -0800

    Giving a base implementation of the shared mapper

--- runtime/shared_mapper.cc
+++ runtime/shared_mapper.cc
@@ -1,7 +1,12 @@
 
 #include "legion.h"
 
+#include <cassert>
+#include <algorithm>
+
 // This is the default implementation of the mapper interface for the shared low level runtime
+
+#define MAX_STEALS_PERMITTED 4
 
 namespace RegionRuntime {
   namespace HighLevel {
@@ -32,6 +37,24 @@ namespace RegionRuntime {
     Processor Mapper::target_task_steal(const std::set<Processor> &blacklist)
     //--------------------------------------------------------------------------------------------
     {
+      // Chose a random processor not on the blacklist
+      const std::set<Processor> &all_procs = machine->get_all_processors();
+      std::set<Processor> diff_procs; 
+      std::set_difference(all_procs.begin(),all_procs.end(),
+                          blacklist.begin(),blacklist.end(),std::inserter(diff_procs,diff_procs.end()));
+      if (diff_procs.empty())
+      {
+        return local_proc;
+      }
+      unsigned index = (rand()) % (diff_procs.size());
+      for (std::set<Processor>::const_iterator it = all_procs.begin();
+            it != all_procs.end(); it++)
+      {
+        if (!index--)
+          return *it;
+      }
+      // Should never make it here
+      assert(false);
       return local_proc;
     }
 
@@ -40,7 +63,19 @@ namespace RegionRuntime {
                                                     std::set<const Task*> &to_steal)
     //--------------------------------------------------------------------------------------------
     {
-
+      unsigned total_stolen = 0;
+      for (std::vector<const Task*>::const_iterator it = tasks.begin();
+            it != tasks.end(); it++)
+      {
+        // Only allow tasks to be stolen one time
+        if (!(*it)->stolen)
+        {
+          to_steal.insert(*it);
+          total_stolen++;
+          if (total_stolen == MAX_STEALS_PERMITTED)
+            break;
+        }
+      }
     }
 
     //--------------------------------------------------------------------------------------------
@@ -48,7 +83,7 @@ namespace RegionRuntime {
                                                       std::vector<IndexSplit> &chunks)
     //--------------------------------------------------------------------------------------------
     {
-
+      // TODO: figure out how to do this easily
     }
 
     //--------------------------------------------------------------------------------------------
@@ -57,7 +92,12 @@ namespace RegionRuntime {
                                   std::vector<Memory> &target_ranking, bool &enable_WAR_optimization)
     //--------------------------------------------------------------------------------------------
     {
-
+      // Try putting it in the local memory, if that doesn't work, try the global memory
+      Memory local = { local_proc.id + 1 };
+      Memory global = { 1 };
+      target_ranking.push_back(local);
+      target_ranking.push_back(global);
+      enable_WAR_optimization = true;
     }
 
     //--------------------------------------------------------------------------------------------
@@ -66,7 +106,9 @@ namespace RegionRuntime {
                                     std::vector<Memory> &future_ranking)
     //--------------------------------------------------------------------------------------------
     {
-
+      // If this is a copy up, put it in the global memory
+      Memory global = { 1 };
+      future_ranking.push_back(global);
     }
 
     //--------------------------------------------------------------------------------------------
@@ -74,7 +116,15 @@ namespace RegionRuntime {
                                     const Memory &dst, Memory &chosen_src)
     //--------------------------------------------------------------------------------------------
     {
-
+      Memory local = { local_proc.id + 1 };
+      if (current_instances.find(local) != current_instances.end())
+      {
+        chosen_src = local;
+      }
+      else
+      {
+        chosen_src = *(current_instances.begin());
+      }
     }
 
     //--------------------------------------------------------------------------------------------
