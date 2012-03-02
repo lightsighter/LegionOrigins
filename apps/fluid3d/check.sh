564195ac5be2b7a1db4d56d53c3dd65189d36cf7
Elliott Slaughter <slaughter@cs.stanford.edu>
2012-03-02 15:54:06 -0800

    Update fluid3d check script for new runtime.

--- apps/fluid3d/check.sh
+++ apps/fluid3d/check.sh
@@ -6,8 +6,8 @@ fluid3d_dir=$(dirname $(readlink -f $BASH_SOURCE))
 # build and run legion
 
 cd $fluid3d_dir
-make fluid3d
-gasnetrun_ibv -n 1 ./fluid3d -ll:csize 16384 -ll:gsize 2000 -level 5 -ll:cpu 1 -s 1 -nbx 1 -nby 1 -nbz 1
+make
+./fluid3d -ll:csize 16384 -ll:gsize 2000 -ll:l1size 16384 -level 4 -ll:cpu 1 -s 1 -nbx 1 -nby 1 -nbz 1
 
 # build parsec
 cd $parsec_dir
@@ -20,4 +20,4 @@ $parsec_exe 1 1 fluid3d_init.fluid fluid3d_parsec.fluid
 
 # compare results
 cd $fluid3d_dir
-$fluid3d_dir/fluid3d_compare.py fluid3d_output.fluid fluid3d_parsec.fluid
+$fluid3d_dir/compare.py fluid3d_output.fluid fluid3d_parsec.fluid
