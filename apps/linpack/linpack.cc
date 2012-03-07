a71fdbb436069124eb4a34a7a83b063797542638
Mike Bauer <mebauer@cs.stanford.edu>
2012-03-06 17:10:07 -0800

    Updating the linpack application to match the new const annotations for tasks

--- apps/linpack/linpack.cc
+++ apps/linpack/linpack.cc
@@ -161,7 +161,7 @@ void randomize_matrix(Context ctx, HighLevelRuntime *runtime,
 
 template<AccessorType AT, int NB>
 void rand_matrix_task(const void *args, size_t arglen,
-		      const std::vector<PhysicalRegion<AT> > &regions,
+		      std::vector<PhysicalRegion<AT> > &regions,
 		      Context ctx, HighLevelRuntime *runtime)
 {
   RandMatrixArgs<NB> *rm_args = (RandMatrixArgs<NB> *)args;
@@ -171,7 +171,7 @@ void rand_matrix_task(const void *args, size_t arglen,
 
 template<AccessorType AT, int NB>
 void linpack_main(const void *args, size_t arglen,
-		  const std::vector<PhysicalRegion<AT> > &regions,
+		  std::vector<PhysicalRegion<AT> > &regions,
 		  Context ctx, HighLevelRuntime *runtime)
 {
   BlockedMatrix<NB> &matrix = *(BlockedMatrix<NB> *)args;
@@ -204,7 +204,7 @@ void do_linpack(Context ctx, HighLevelRuntime *runtime)
 
 template<AccessorType AT>
 void top_level_task(const void *args, size_t arglen,
-		    const std::vector<PhysicalRegion<AT> > &regions,
+		    std::vector<PhysicalRegion<AT> > &regions,
 		    Context ctx, HighLevelRuntime *runtime) {
 
   while (!Config::args_read)
