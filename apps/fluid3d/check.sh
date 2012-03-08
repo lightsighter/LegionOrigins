fe53032e144db477d51a2017a63301da6ec17f42
Elliott Slaughter <slaughter@cs.stanford.edu>
2012-03-07 16:56:10 -0800

    fluid3d: Remove test script's dependence on parsec where possible.

--- apps/fluid3d/check.sh
+++ apps/fluid3d/check.sh
@@ -11,11 +11,15 @@ fluid3d_dir=$(dirname $(readlink -f $BASH_SOURCE))
 
 cd $fluid3d_dir
 make
-tar xf $parsec_dir/pkgs/apps/fluidanimate/inputs/input_test.tar
 cp in_5K.fluid init.fluid
 ./fluid3d -ll:csize 16384 -ll:gsize 2000 -ll:l1size 16384 -level 4 -ll:cpu 1 -s 1 -nbx 1 -nby 1 -nbz 1
 
 # build parsec
+if [[ ! -d $parsec_dir ]]; then
+    echo "Error: Unable to locate parsec for comparison to legion."
+    exit
+fi
+
 cd $parsec_dir
 $parsec_dir/bin/parsecmgmt -a build -p fluidanimate -c gcc
 parsec_exe=$parsec_dir/pkgs/apps/fluidanimate/inst/amd64-linux.gcc/bin/fluidanimate
