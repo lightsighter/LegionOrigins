592f1e7b2ad6c7f7db53c545809f0216affea6eb
Elliott Slaughter <slaughter@cs.stanford.edu>
2012-03-19 16:40:12 -0700

    fluid3d: Checkpoint. Subdivision by z working. Mapper trouble.

--- apps/fluid3d/check.sh
+++ apps/fluid3d/check.sh
@@ -15,7 +15,7 @@ if [[ $rebuild -ne 0 ]]; then
     make
 fi
 cp in_5K.fluid init.fluid
-./fluid3d -ll:csize 16384 -ll:gsize 2000 -ll:l1size 16384 -level 1 -ll:cpu 1 -s 1 -nbx 2 -nby 1 -nbz 1 2>&1 | tee legion.out
+./fluid3d -ll:csize 16384 -ll:gsize 2000 -ll:l1size 16384 -level 4 -ll:cpu 1 -s 1 -nbx 1 -nby 1 -nbz 2 2>&1 | tee legion.log
 if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
     exit
 fi
@@ -27,18 +27,18 @@ if [[ ! -d $parsec_dir ]]; then
 fi
 
 cd $parsec_dir
-parsec_src=$parsec_dir/pkgs/apps/fluidanimate/src/pthreads.cpp
-parsec_exe=$parsec_dir/pkgs/apps/fluidanimate/inst/amd64-linux.gcc/bin/fluidanimate
+parsec_src=$parsec_dir/pkgs/apps/fluidanimate/src/serial.cpp
+parsec_exe=$parsec_dir/pkgs/apps/fluidanimate/inst/amd64-linux.gcc-serial/bin/fluidanimate
 if [[ $parsec_src -nt $parsec_exe ]]; then
     $parsec_dir/bin/parsecmgmt -a fullclean
     $parsec_dir/bin/parsecmgmt -a fulluninstall
-    $parsec_dir/bin/parsecmgmt -a build -p fluidanimate
+    $parsec_dir/bin/parsecmgmt -a build -p fluidanimate -c gcc-serial
 fi
 
 # run parsec
 cd $fluid3d_dir
 # <threadnum> <framenum> <.fluid input file> [.fluid output file]
-$parsec_exe 1 1 init.fluid parsec.fluid 2>&1 | tee parsec.out
+$parsec_exe 1 1 init.fluid parsec.fluid 2>&1 | tee parsec.log
 if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
     exit
 fi
