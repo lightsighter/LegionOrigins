91d310beb7aaa81a4643d05709e791c30d17c2ba
Elliott Slaughter <elliottslaughter@gmail.com>
2012-04-24 17:11:55 -0700

    Revert "fluid3d: Trying to make scripts run on viz."
    This reverts commit a1b22b81450cafe47ba8ae251f0e96bc3d286fb3.

--- apps/fluid3d/compare.py
+++ apps/fluid3d/compare.py
@@ -18,12 +18,11 @@ def read_particle(f):
 
 def read_file(filename):
     content = {}
-    f = open(filename, 'rb')
+    with open(filename, 'rb') as f:
         content['rest_particles_per_meter'] = read_float32(f)
         content['orig_num_particles'] = read_int32(f)
         content['particles'] = [read_particle(f)
                                 for p in xrange(content['orig_num_particles'])]
-    f.close()
     return content
 
 def summarize(filename, parts):
