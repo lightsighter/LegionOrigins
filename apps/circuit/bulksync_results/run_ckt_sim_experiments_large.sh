e31b654788e1668ef64950611dd7f2ac2d3656b1
Michael Bauer <sequoia@kidlogin2.nics.utk.edu>
2012-07-15 16:41:03 -0400

    Adding results of bulk synchronous experiments on keeneland

---
+++ apps/circuit/bulksync_results/run_ckt_sim_experiments_large.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cd /nics/d/home/sequoia/mike_legion/region/apps/circuit/bulksync_results/
+cat $PBS_NODEFILE | uniq > /nics/d/home/sequoia/mike_legion/region/apps/circuit/bulksync_results//unique_big2.txt
+export GASNET_SSH_NODEFILE=/nics/d/home/sequoia/mike_legion/region/apps/circuit/bulksync_results//unique_big2.txt
+
+#echo "48 pieces 16 nodes 1 cpus 1 gpus"
+#GASNET_BACKTRACE=1 gasnetrun_ibv -n 16 /nics/d/home/sequoia/mike_legion/region/apps/circuit/ckt_sim -ll:cpu 1 -ll:gpu 1 -ll:dma 2 -ll:zsize 1536 -ll:fsize 1024 -ll:csize 4096 -ll:gsize 1536 -npp 2500 -wpp 16384 -p 48 -l 10 -level 3 1> ckt_sim_48_16_1_1.stdio 2> ckt_sim_48_16_1_1.stderr 
+
+#echo "48 pieces 16 nodes 1 cpus 2 gpus"
+#GASNET_BACKTRACE=1 gasnetrun_ibv -n 16 /nics/d/home/sequoia/mike_legion/region/apps/circuit/ckt_sim -ll:cpu 1 -ll:gpu 2 -ll:dma 2 -ll:zsize 1536 -ll:fsize 1024 -ll:csize 4096 -ll:gsize 1536 -npp 2500 -wpp 16384 -p 48 -l 10 -level 3 1> ckt_sim_48_16_1_2.stdio 2> ckt_sim_48_16_1_2.stderr 
+
+#echo "48 pieces 16 nodes 1 cpus 3 gpus"
+#GASNET_BACKTRACE=1 gasnetrun_ibv -n 16 /nics/d/home/sequoia/mike_legion/region/apps/circuit/ckt_sim -ll:cpu 1 -ll:gpu 3 -ll:dma 2 -ll:zsize 1536 -ll:fsize 1024 -ll:csize 4096 -ll:gsize 1536 -npp 2500 -wpp 16384 -p 48 -l 10 -level 3 -sync 1 1> ckt_sim_48_16_1_3.stdio 2> ckt_sim_48_16_1_3.stderr 
+
+#echo "96 pieces 16 nodes 1 cpus 1 gpus"
+#GASNET_BACKTRACE=1 gasnetrun_ibv -n 16 /nics/d/home/sequoia/mike_legion/region/apps/circuit/ckt_sim -ll:cpu 1 -ll:gpu 1 -ll:dma 2 -ll:zsize 1536 -ll:fsize 1024 -ll:csize 4096 -ll:gsize 1536 -npp 2500 -wpp 16384 -p 96 -l 10 -level 3 1> ckt_sim_96_16_1_1.stdio 2> ckt_sim_96_16_1_1.stderr 
+
+#echo "96 pieces 16 nodes 1 cpus 2 gpus"
+#GASNET_BACKTRACE=1 gasnetrun_ibv -n 16 /nics/d/home/sequoia/mike_legion/region/apps/circuit/ckt_sim -ll:cpu 1 -ll:gpu 2 -ll:dma 2 -ll:zsize 1536 -ll:fsize 1024 -ll:csize 4096 -ll:gsize 1536 -npp 2500 -wpp 16384 -p 96 -l 10 -level 3 1> ckt_sim_96_16_1_2.stdio 2> ckt_sim_96_16_1_2.stderr 
+
+#echo "96 pieces 16 nodes 1 cpus 3 gpus"
+#GASNET_BACKTRACE=1 gasnetrun_ibv -n 16 /nics/d/home/sequoia/mike_legion/region/apps/circuit/ckt_sim -ll:cpu 1 -ll:gpu 3 -ll:dma 2 -ll:zsize 1536 -ll:fsize 1024 -ll:csize 4096 -ll:gsize 1536 -npp 2500 -wpp 16384 -p 96 -l 10 -level 3 -sync 1 1> ckt_sim_96_16_1_3.stdio 2> ckt_sim_96_16_1_3.stderr 
+
+#echo "96 pieces 32 nodes 1 cpus 1 gpus"
+#GASNET_BACKTRACE=1 gasnetrun_ibv -n 32 /nics/d/home/sequoia/mike_legion/region/apps/circuit/ckt_sim -ll:cpu 1 -ll:gpu 1 -ll:dma 2 -ll:zsize 1536 -ll:fsize 1024 -ll:csize 4096 -ll:gsize 1536 -npp 2500 -wpp 16384 -p 96 -l 10 -level 3 1> ckt_sim_96_32_1_1.stdio 2> ckt_sim_96_32_1_1.stderr 
+
+#echo "96 pieces 32 nodes 1 cpus 2 gpus"
+#GASNET_BACKTRACE=1 gasnetrun_ibv -n 32 /nics/d/home/sequoia/mike_legion/region/apps/circuit/ckt_sim -ll:cpu 1 -ll:gpu 2 -ll:dma 2 -ll:zsize 1536 -ll:fsize 1024 -ll:csize 4096 -ll:gsize 1536 -npp 2500 -wpp 16384 -p 96 -l 10 -level 3 1> ckt_sim_96_32_1_2.stdio 2> ckt_sim_96_32_1_2.stderr 
+
+echo "96 pieces 32 nodes 1 cpus 3 gpus"
+GASNET_BACKTRACE=1 gasnetrun_ibv -n 32 /nics/d/home/sequoia/mike_legion/region/apps/circuit/ckt_sim -ll:cpu 1 -ll:gpu 3 -ll:dma 4 -ll:zsize 1536 -ll:fsize 1024 -ll:csize 4096 -ll:gsize 1536 -npp 2500 -wpp 16384 -p 96 -l 10 -level 3 -sync 1 1> ckt_sim_96_32_1_3.stdio 2> ckt_sim_96_32_1_3.stderr 
+
