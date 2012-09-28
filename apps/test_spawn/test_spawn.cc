59707084cba680b0b7277e35696d6ef62c14da3d
Elliott Slaughter <slaughter@cs.stanford.edu>
2012-09-28 15:59:01 -0700

    Improve task spawn test.

--- apps/test_spawn/test_spawn.cc
+++ apps/test_spawn/test_spawn.cc
@@ -19,6 +19,7 @@ const unsigned DEFAULT_DEPTH = 10;
 const unsigned DEFAULT_SPREAD = 10;
 
 struct Args {
+  long expected_result;
   unsigned depth;
   unsigned spread;
 };
@@ -55,22 +56,23 @@ void top_level_task(const void *args, size_t arglen,
   struct timespec ts_start, ts_end;
   clock_gettime(CLOCK_MONOTONIC, &ts_start);
 
+  child_args.expected_result = random();
   Future f = runtime->execute_task(ctx, TASKID_RECURSE, indexes, fields, child_regions,
 				   TaskArgument(&child_args, sizeof(Args)));
-  f.get_void_result();
+  long result = f.get_result<long>();
+  assert(result == child_args.expected_result);
 
   clock_gettime(CLOCK_MONOTONIC, &ts_end);
   double sim_time = ((1.0 * (ts_end.tv_sec - ts_start.tv_sec)) +
                      (1e-9 * (ts_end.tv_nsec - ts_start.tv_nsec)));
   printf("ELAPSED TIME = %7.3f s\n", sim_time);
   RegionRuntime::DetailedTimer::report_timers();
-
 }
 
-void recurse_task(const void *args, size_t arglen,
-                    const std::vector<RegionRequirement> &reqs,
-                    const std::vector<PhysicalRegion> &regions,
-                    Context ctx, HighLevelRuntime *runtime) {
+long recurse_task(const void *args, size_t arglen,
+                  const std::vector<RegionRequirement> &reqs,
+                  const std::vector<PhysicalRegion> &regions,
+                  Context ctx, HighLevelRuntime *runtime) {
   Args &my_args = *(Args *)args;
 
   Args child_args;
@@ -82,11 +84,27 @@ void recurse_task(const void *args, size_t arglen,
   std::vector<RegionRequirement> child_regions;
 
   if (my_args.depth > 0) {
-    for (unsigned i = 0; i < my_args.spread; i++) {
+    unsigned long num_children = random() % my_args.spread;
+    std::vector<unsigned long> expected_results;
+    std::vector<Future> results;
+    for (unsigned i = 0; i < num_children; i++) {
+      unsigned long expected_result = random();
+      child_args.expected_result = expected_result;
+      expected_results.push_back(expected_result);
       Future f = runtime->execute_task(ctx, TASKID_RECURSE, indexes, fields, child_regions,
                                        TaskArgument(&child_args, sizeof(Args)));
+      results.push_back(f);
+    }
+    while(!expected_results.empty() && !results.empty()) {
+      Future f = results.back();
+      unsigned long expected_result = expected_results.back();
+      results.pop_back();
+      expected_results.pop_back();
+      assert(f.get_result<long>() == expected_result);
     }
   }
+
+  return child_args.expected_result;
 }
 
 void create_mappers(Machine *machine, HighLevelRuntime *runtime,
@@ -95,12 +113,15 @@ void create_mappers(Machine *machine, HighLevelRuntime *runtime,
 }
 
 int main(int argc, char **argv) {
-  srand(time(NULL));
+  // Use high-precision clock to get more variation among runs started at around the same time.
+  struct timespec ts;
+  clock_gettime(CLOCK_REALTIME, &ts);
+  srandom(ts.tv_nsec);
 
   HighLevelRuntime::set_registration_callback(create_mappers);
   HighLevelRuntime::set_top_level_task_id(TOP_LEVEL_TASK_ID);
   HighLevelRuntime::register_single_task<top_level_task>(TOP_LEVEL_TASK_ID, Processor::LOC_PROC, false, "top_level_task");
-  HighLevelRuntime::register_single_task<recurse_task>(TASKID_RECURSE, Processor::LOC_PROC, false, "recurse_task");
+  HighLevelRuntime::register_single_task<long, recurse_task>(TASKID_RECURSE, Processor::LOC_PROC, false, "recurse_task");
 
   return HighLevelRuntime::start(argc, argv);
 }
